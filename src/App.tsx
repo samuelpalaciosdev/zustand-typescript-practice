b0e017929bb3de33b05d1bc28f7d1ca0eb0f67e1
Samuel <samuelpl0888@gmail.com>
2023-02-22 03:59:04 -0400

    Add global state fetch function

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
+import { useEffect } from 'react';
 import { useCounterStore } from './store/counterStore';
 import { shallow } from 'zustand/shallow';
 
 const App = () => {
-  const { count } = useCounterStore();
-  const { increment } = useCounterStore();
+  const { count, posts } = useCounterStore(
+    state => ({
+      count: state.count,
+      posts: state.posts,
+    }),
+    shallow
+  );
 
-  console.log(count);
+  const { increment, getPosts } = useCounterStore();
+
+  useEffect(() => {
+    getPosts();
+  }, []);
 
   return (
     <>
       <h1>Hello world</h1>
       <h2>Count is: {count} </h2>
       <button onClick={() => increment(10)}>Increment by 10</button>
+      {JSON.stringify(posts)}
     </>
   );
 };
--- src/store/counterStore.tsx
+++ src/store/counterStore.tsx
@@ -1,14 +1,27 @@
 import { create } from 'zustand';
 
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
 interface CounterState {
   count: number;
+  posts: Post[];
   increment: (value: number) => void;
+  getPosts: () => Promise<void>;
 }
 
 export const useCounterStore = create<CounterState>(set => ({
   count: 10,
   increment: (value: number) =>
-    set(state => ({
-      count: state.count + value,
-    })),
+    set(state => ({ ...state, count: state.count + value })),
+  posts: [],
+  getPosts: async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await res.json();
+    set(state => ({ ...state, posts }));
+    console.log(posts);
+  },
 }));
